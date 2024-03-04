e467d8bb4667e2e200567c987a4ef515c8653d1f
Glend Maatita <glend.maatita@gmail.com>
2024-03-04 20:17:57 +0700

    example of terraform

--- terraform/9-sql.tf
+++ terraform/9-sql.tf
@@ -1,13 +1,64 @@
 # DB Instance
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance
+resource "google_sql_database_instance" "onxp-sql" {
+  name             = "onxp-sql"
+  region           = var.region
+  database_version = "POSTGRES_15_6"
 
+  depends_on = [google_service_networking_connection.private_vpc_connection_support]
+
+  settings {
+    tier = "db-f1-micro"
+    availability_type = "ZONAL"
+    disk_autoresize = true
+    disk_size = 10
+    disk_type = "PD_SSD"
+
+    backup_configuration {
+      enabled = true
+      location = var.region
+      transaction_log_retention_days = 7
+
+      backup_retention_settings {
+        retained_backups = 7
+      }
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+      private_network = google_compute_network.main.id
+
+      authorized_networks {
+        name = "public"
+        value = "0.0.0.0/0"
+      }
+    }
+
+    deletion_protection_enabled = true
+
+    insights_config {
+      query_insights_enabled = true
+    }
+  }
+}
 
 # Database
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database
 
+resource "google_sql_database" "onxp-production" {
+  name = "onxp-production"
+  instance = google_sql_database_instance.onxp-sql.name
+}
+
 
 # Create DB User
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_user
+
+resource "google_sql_user" "onxp-user" {
+  name = "onxp"
+  password = "onxpsecret"
+  instance = google_sql_database_instance.onxp-sql.name
+}
 
 
 # [Recommendation] Create Service Account for CloudSQL
