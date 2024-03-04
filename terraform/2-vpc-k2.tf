e26d6dc21efd932e124ffe458c01ed8dae01f950
Najmi Najib Nasrulloh Kurniadi <najminajibnk@gmail.com>
2024-03-04 20:17:16 +0700

    comment out google api

--- terraform/2-vpc-k2.tf
+++ terraform/2-vpc-k2.tf
@@ -1,13 +1,13 @@
 # VPC
 
 # Enable Google API
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
-}
+# resource "google_project_service" "compute" {
+#   service = "compute.googleapis.com"
+# }
 
-resource "google_project_service" "container" {
-  service = "container.googleapis.com"
-}
+# resource "google_project_service" "container" {
+#   service = "container.googleapis.com"
+# }
 
 # Create a VPC network
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
