0e2e32a535d4c698b296629c7ba133867596d8bf
Adhika WK <adhikawk@gmail.com>
2024-03-08 18:30:17 +0700

    betulin vpc config

--- terraform/2-vpc-kel1.tf
+++ terraform/2-vpc-kel1.tf
@@ -1,13 +1,13 @@
 # VPC
 
 # Enable Google API
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
-}
+#resource "google_project_service" "compute" {
+#  service = "compute.googleapis.com"
+#}
 
-resource "google_project_service" "container" {
-  service = "container.googleapis.com"
-}
+#resource "google_project_service" "container" {
+#  service = "container.googleapis.com"
+#}
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "vpc-kel1" {
