339af8eaf51dca44d0a56f17e58667d45d089411
Adhika WK <adhikawk@gmail.com>
2024-03-08 18:22:35 +0700

    kelompok 1 terraform challange

---
+++ terraform/8-node-pool-kel1.tf
@@ -0,0 +1,38 @@
+# [Recommendation] Service Account for Node Pool
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "kubernetes-kel1-sa" {
+  account_id = "kubernetes-kel1-sa"
+}
+
+# Node Pool
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
+resource "google_container_node_pool" "kel1-node-pool" {
+    name = "kel1-node-pool"
+    cluster = google_container_cluster.kel1-kubernetes
+    node_count = 1
+
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 10
+      location_policy = "BALANCED"
+    }
+
+    management {
+      auto_repair = true
+      auto_upgrade = true
+    }
+
+    node_locations = ["us-central1-a"]
+
+    node_config {
+      preemptible = true
+      machine_type = "e2.micro"
+      disk_size_gb = 80
+      disk_type = "pd-balanced"
+
+      service_account = google_service_account.kubernetes-kel1-sa
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform"
+      ]
+    }
+}
---
+++ terraform/variables-kel1.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "Project ID"
+  type = string
+  default = "kelompok1"
+}
+
+variable "region" {
+  description = "Region of project"
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  description = "Zone of project"
+  type = string
+  default = "us-central1-c"
+}
