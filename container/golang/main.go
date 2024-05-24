6b9fb46b61b7e7ced90278213efda63d8d021f16
Glend Maatita <glend.maatita@gmail.com>
2024-05-24 16:41:03 +0700

    fix go lang example

--- container/golang/main.go
+++ container/golang/main.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-	"context"
-	"log"
-	"os"
-	"strings"
-
-	"github.com/SigNoz/sample-golang-app/controllers"
-	"google.golang.org/grpc/credentials"
-
-	"github.com/gin-gonic/gin"
-)
-
-var (
-	serviceName  = os.Getenv("SERVICE_NAME")
-	insecure     = os.Getenv("INSECURE_MODE")
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-
-	r := gin.Default()
-
-	// Routes
-	r.GET("/hello", controllers.Hello)
-
-	// Run the server
-	r.Run(":8090")
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "msg": "Hello, World!",
+        })
+    })
+    r.Run() // listen and serve on 0.0.0.0:8080
 }
